ec62d115a5e4720337197efa576fd56d231aac94
SeiichiN <billie175@gmail.com>
2025-07-01 16:23:32 +0900

    7/1 次はアプリケーションスコープ

---
+++ chap08/src/main/java/model/RegisterUserLogic.java
@@ -0,0 +1,9 @@
+package model;
+
+public class RegisterUserLogic {
+	public boolean execute(User user) {
+		System.out.println("データベースに保存しました");
+		
+		return true;
+	}
+}
---
+++ chap08/src/main/java/servlet/RegisterUser.java
@@ -0,0 +1,56 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import model.RegisterUserLogic;
+import model.User;
+
+@WebServlet("/RegisterUser")
+public class RegisterUser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getParameter("action");
+		String path = null;
+		if (action == null) {
+			path = "WEB-INF/jsp/registerForm.jsp";
+		} else if (action.equals("done")) {
+			HttpSession session = request.getSession();
+			User registerUser = (User)session.getAttribute("registerUser");
+			
+			RegisterUserLogic logic = new RegisterUserLogic();
+			logic.execute(registerUser);
+			
+			session.removeAttribute("registerUser");
+			System.out.println("セッションスコープから削除しました");
+			
+			path = "WEB-INF/jsp/registerDone.jsp";
+		}
+		RequestDispatcher d = request.getRequestDispatcher(path);
+		d.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String id = request.getParameter("id");
+		String pass = request.getParameter("pass");
+		String name = request.getParameter("name");
+		
+		User registerUser = new User(id, pass, name);
+		HttpSession session = request.getSession();
+		session.setAttribute("registerUser", registerUser);
+		
+		String path = "WEB-INF/jsp/registerConfirm.jsp";
+		RequestDispatcher d = request.getRequestDispatcher(path);
+		d.forward(request, response);
+	}
+
+}
